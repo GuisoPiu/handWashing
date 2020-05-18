be51267a525d73267b64af11968e13d787689946
Hector Rios <gruposiice.hr@gmail.com>
2020-05-18 18:40:18 -0500

    committing HTML

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,21 @@
 <script>
-
+    import HowTo from './howTo.svelte';
+    import Timer from './timer.svelte';
 </script>
 
-<h1>Test LATO</h1>
+<h1>Hand Washing App</h1>
+
+<Timer />
+<HowTo />
+
+<h3>
+  <a href="https://vid.alarabiya.net/images/2020/03/22/1ff220bb-03ef-4911-b8e1-32f72eed355e/1ff220bb-03ef-4911-b8e1-32f72eed355e.PNG">Picture source</a>
+  <a href="https://english.alarabiya.net/en/coronavirus/2020/03/22/Coronavirus-Hand-washing-is-important-here-s-how-to-do-it-effectively">Sound source</a>
+</h3>
 
 
 <style>
-
+    h1, h3 {
+        text-align: center
+    }
 </style>
